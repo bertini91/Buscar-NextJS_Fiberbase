fe7efa152ce4650856f58291f83d9b711228c3f6
Nicolas Bertini Argañaras <nicolas_bertini@hotmail.com>
2021-04-24 12:14:44 -0300

    Listado de productos con sus respectivos styled components

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,52 @@
 import styled from "@emotion/styled";
+import { useContext, useEffect, useState } from "react";
+import DetalleProducto from "../components/layout/DetalleProducto";
 import Layout from "../components/layout/Layout";
+import { FirebaseContext } from "../firebase";
 
 const Heading = styled.h1`
   color: red;
 `;
 
 export default function Home() {
+  const [productos, setProductos] = useState([]);
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    const obtenerProductos = () => {
+      firebase.db
+        .collection("productos")
+        .orderBy("creado", "desc")
+        .onSnapshot(manejarSnapshot);
+    };
+    obtenerProductos();
+  }, []);
+
+  function manejarSnapshot(snapshot) {
+    const productos = snapshot.docs.map((doc) => {
+      return {
+        id: doc.id,
+        ...doc.data(),
+      };
+    });
+    setProductos(productos);
+  }
+
   return (
     <div>
       <Layout>
-        <Heading>Inciio</Heading>
+        <div className="listado-productos">
+          <div className="contenedor">
+            <ul className="bg-white">
+              {productos.map((producto) => (
+                <DetalleProducto
+                  key={producto.id}
+                  producto={producto}
+                ></DetalleProducto>
+              ))}
+            </ul>
+          </div>
+        </div>
       </Layout>
     </div>
   );
